a13ea5d2c19aae34b8f748556440c35f1d28b9d2
Joby Poriyath <joby.poriyath@citrix.com>
2014-05-06 09:04:33 +0100

    Starting with List...

---
+++ c++/lst.cc
@@ -0,0 +1,11 @@
+#include "lst.h"
+
+int main()
+{
+	return 0;
+}
+
+// Local Variables:
+// c-basic-offset: 8
+// mode: c++
+// End:
---
+++ c++/lst.h
@@ -0,0 +1,41 @@
+#ifndef __HEADER_LST__H_
+#define __HEADER_LST__H_
+
+#include <cstdlib>
+#include <algorithm>
+
+/*
+  Lst<int> v;
+  Lst<int> v(100);
+  Lst<int> v1(v.begin(), v.end())
+ 
+  v1.size()
+  v1.push_back()
+  v1.push_front()
+  
+  v1.pop_back()
+  v1.pop_front()
+
+  v1.erase(it)
+  v1.erase(b, e)
+
+  v1.clear()
+
+  
+ */
+
+
+template <typename T>
+class Lst {
+public:
+	typedef 
+
+private:
+	
+};
+
+#endif
+// Local Variables:
+// c-basic-offset: 8
+// mode: c++
+// End:
