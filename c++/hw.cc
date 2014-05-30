f5219f9c5fb71139948ceadb43d5da67b1764b66
Joby Poriyath <joby.poriyath@citrix.com>
2014-05-30 19:21:24 +0100

    playing with threads

--- c++/hw.cc
+++ c++/hw.cc
@@ -8,13 +8,41 @@ using std::thread;
 
 void hello()
 {
-	cout << "Hello, world" << endl;
+	cout << "Hello, world\n";
 }
+
+class thread_guard {
+
+public:
+	explicit thread_guard(thread& _t) : t( _t) {}
+
+	~thread_guard() 
+	{
+		cout << __PRETTY_FUNCTION__ << endl;
+		if ( t.joinable()) {
+
+			t.join();
+			cout << "you should never see this\n";
+		}
+	}
+
+	thread_guard(const thread_guard&) = delete;
+	thread_guard& operator=(const thread_guard&) = delete;
+	
+private:
+	thread& t;
+};
 
 int main()
 {
-	thread t(hello);
-	t.join();
+	thread x( hello);
+	thread_guard tx( x);
+	
+	throw std::runtime_error("murpy's law!");
+	
+	
+	cout << ( x.joinable() ? "true" : "false") << "\n";
+	//x.join();
 	
 	return 0;
 }
