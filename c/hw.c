bdcc8e086cf2e3d4073769ef7cde05d362c74531
Joby Poriyath <joby.poriyath@citrix.com>
2014-06-24 04:54:44 +0100

    inet_pton and inet_ntop

--- c/hw.c
+++ c/hw.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <arpa/inet.h>
+
 int main()
 {
-	printf("%ld\n", sizeof(uint32_t));
+	struct sockaddr_in sa;
+	char ip4[INET_ADDRSTRLEN];
+	struct sockaddr_in6 sa6;
+	char ip6[INET6_ADDRSTRLEN];
+	
+	if ( inet_pton( AF_INET, 
+			"192.168.0.3", 
+			&(sa.sin_addr)) != 1)
+		return 1;
+
+	if ( inet_ntop( AF_INET, 
+			&(sa.sin_addr), 
+			ip4, 
+			INET_ADDRSTRLEN) == ip4)
+		printf("%s\n", ip4);
+	else
+		return 1;
+
+	if ( inet_pton( AF_INET6, 
+			"fe80::2202:afff:fedf:6dce", 
+			&(sa6.sin6_addr)) != 1)
+		return 1;
+
+	if ( inet_ntop( AF_INET6, 
+			&(sa6.sin6_addr), 
+			ip6, 
+			INET6_ADDRSTRLEN) == ip6)
+		printf("%s\n", ip6);
+	else
+		return 1;
+
 	return 0;
 }
