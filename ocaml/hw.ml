c77919ce2616515e4c6da0efeea24b1200ac01d3
Joby Poriyath <joby.poriyath@citrix.com>
2014-08-12 09:18:25 +0100

    scratch file...

--- ocaml/hw.ml
+++ ocaml/hw.ml
@@ -81,7 +81,42 @@ let rec read_and_accumulate accum =
   | Some x -> read_and_accumulate (accum +. Float.of_string x)
   | None -> accum
 
-let () = 
-  printf "Total: %F\n" (read_and_accumulate 0.)
+(* let () =  *)
+(*   printf "Total: %F\n" (read_and_accumulate 0.) *)
 
+
+let languages = "OCaml,Perl,C++,C"
+
+let dashed_languages = 
+  let language_list = String.split languages ~on:',' in
+  String.concat language_list ~sep:"-"
+
+
+let area_of_ring inner_radius outer_radius =
+  let pi = acos(-1.) in
+  let area_of_circle r = pi *. r *. r in
+  area_of_circle outer_radius -. area_of_circle inner_radius
+
+let enumerate list = 
+  let zip = List.zip (List.range 0 (List.length list)) list in
+  match zip with
+  | Some z' -> z'
+  | None -> []
+
+let upcase_first_entry line = 
+  match String.split line ~on:',' with
+  | first :: rest -> String.concat ~sep:"," (String.uppercase first :: rest)
+  | [] -> assert false (* String.split returns atleast one element *)
+
+let rec find_first_stutter list = 
+  match list with
+  | x :: y :: tl -> 
+     if x = y then Some x else find_first_stutter (y :: tl)
+  | [_] 
+  | [] -> None  (* Only zero or one elements, so no repeats *)
+
+let rec is_even x = 
+  if x = 0 then true else is_odd (x - 1)
+and is_odd x =
+  if x = 0 then false else is_even (x - 1)
 
