6eef8a96ded1f8e67cb23c979748ce224f233280
Joby Poriyath <joby.poriyath@citrix.com>
2014-10-23 13:35:40 +0100

    picking up bit of assembly

--- asm/exit.asm
+++ asm/exit.asm
@@ -1,14 +1,30 @@
-	segment .data
+	.section .rodata
+my_name:
+	.string "Joby Poriyath"
 
-a	dq	175
-b	dq	4097
-
-	segment .text
-	global	main
-
-main:
-	mov	rax,	[a]
-	add	rax,	[b]
-	xor	rax,	rax
-	ret
+format_str_1:
+	.string "I'm %s"
 	
+	.section .text
+	.globl main
+main:
+	pushq %rbp
+	movq  %rsp, %rbp
+
+	subq  $1024, %rsp
+
+	movq  %rsp, %rdi
+	movq  $1024, %rsi
+	movq  $format_str_1, %rdx
+	movq  $my_name, %rcx
+	xorl %eax, %eax
+	call snprintf
+	
+	movq %rsp, %rdi
+	xorl %eax, %eax
+	call puts
+
+	xorl %eax, %eax
+	movq %rbp, %rsp
+	popq %rbp
+	ret
