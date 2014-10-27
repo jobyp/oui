4a3568aef4b12c30213f09e0c9c5b4ef3cd1439f
Dina E Thomas <dinaelizathomas@gmail.com>
2014-10-27 20:19:25 +0000

    Playing with x86-64 assembly

--- asm/exit.asm
+++ asm/exit.asm
@@ -1,30 +1,20 @@
-	.section .rodata
-my_name:
-	.string "Joby Poriyath"
+	.section .data
+a:	.quad 4
+b:	.double 4.4
+c:	.rept 10
+	.long 0
+	.endr
+d:	.word 1, 2
+e:	.byte 0xfb
+f:	.string "Hello, World"
 
-format_str_1:
-	.string "I'm %s"
-	
 	.section .text
 	.globl main
 main:
 	pushq %rbp
 	movq  %rsp, %rbp
-
-	subq  $1024, %rsp
-
-	movq  %rsp, %rdi
-	movq  $1024, %rsi
-	movq  $format_str_1, %rdx
-	movq  $my_name, %rcx
-	xorl %eax, %eax
-	call snprintf
-	
-	movq %rsp, %rdi
-	xorl %eax, %eax
-	call puts
+	subq $16, %rsp
 
 	xorl %eax, %eax
-	movq %rbp, %rsp
-	popq %rbp
+	leave
 	ret
