d79895235b9ef78225a16ea27ae05527e3d73010
Joby Poriyath <joby.poriyath@citrix.com>
2015-02-17 13:09:04 +0000

    OCaml: higher order functions (Ch 6)

--- ocaml/hw.ml
+++ ocaml/hw.ml
@@ -1,67 +1,78 @@
-open Core.Std
-
-let lang_info_header = ["language"; "architect"; "first release"]
-
-let lang_info = [
-    ["Lisp";  "John McCarthy";   "1958"];
-    ["C";     "Dennis Ritchie";  "1969"];
-    ["ML";    "Robin Milner";    "1973"];
-    ["OCaml"; "Xavier Leroy";    "1996"];
-  ]
-
-let rev = List.fold ~init:[] ~f:(fun accum x -> x :: accum)
-
-let max_widths header rows = 
-  let lengths l = List.map ~f:String.length l in
-  let f2 = List.map2_exn ~f:Int.max in
-  List.fold ~init:(lengths header) ~f:(fun acc row -> f2 acc (lengths row)) rows
-
-let render_seperator widths = 
-  let peices = List.map ~f:(fun w -> String.make (w + 2) '-') widths in
-  "|" ^ String.concat ~sep:"+" peices ^ "|" 
-
-let pad s length = 
-  " " ^ s ^ String.make (length - String.length s + 1) ' '
-
-let render_row row widths = 
-  let padded = List.map2_exn row widths ~f:pad in
-  "|" ^ String.concat ~sep:"|" padded ^ "|" 
-
-let render_table header rows = 
-  let widths = max_widths header rows in
-  String.concat ~sep:"\n" 
-		(render_row header widths
-		 :: render_seperator widths 
-		 :: List.map ~f:(fun row -> render_row row widths) rows)
-		
-let () = 
-  printf "%s\n" (render_table lang_info_header lang_info)
-
-(*
- *  List.{map,map2_exn,fold,dedup,reduce,partition_tf,append
- *
- *  List.filter_map drops all the elements for which None is returned
- *)
-
-
-let is_ocaml_source s = 
-  match String.rsplit2 s ~on:'.' with
-  | Some ("", _) -> false
-  | Some (_, ("ml"|"mli")) -> true
-  | _ -> false
-
-
-let rec ls_rec s = 
-  if Sys.is_file_exn ~follow_symlinks:true s
-  then [s]
+let sort l =
+  let rec insert n l = 
+    match l with
+    | [] -> [n]
+    | h :: t -> 
+      if n > h 
+      then h :: (insert n t)
+      else n :: l
+  in
+  let rec sort_aux l =
+    match l with
+    | [] -> []
+    | h :: t -> insert h (sort_aux t)
+  in
+  sort_aux l
+      
+let rec take n l = 
+  if n <= 0
+  then []
   else 
-    Sys.ls_dir s
-    |> List.map ~f:(fun sub -> ls_rec (s ^/ sub))
-    |> List.concat
+    match l with
+    | [] -> []
+    | h :: t -> h :: take (n - 1) t
 
-let rec destutter = function
-  | [] as l -> l
-  | [_] as l -> l
-  | hd :: (hd' :: _ as tl) when hd = hd' -> destutter tl
-  | hd :: tl -> hd :: destutter tl
+let rec drop n l = 
+  if n <= 0
+  then l
+  else 
+    match l with
+    | [] -> []
+    | _ :: t -> drop (n - 1) t
+
+let rec merge x y = 
+  match x, y with
+  | [], [] -> []
+  | [], l -> l
+  | l, [] -> l
+  | (hx :: tx), (hy :: ty) ->
+    if hx <= hy
+    then hx :: merge tx (hy :: ty)
+    else hy :: merge (hx :: tx) ty
+
+let rec msort l =
+  match l with
+  | [] -> []
+  | [x] -> [x]
+  | _ ->
+    let mid = List.length l / 2 in
+    let left = take mid l in
+    let right = drop mid l in
+    merge (msort left) (msort right)
+
+let rec detect l = 
+  match l with
+  | [] -> true
+  | [x] -> true
+  | h :: x :: t ->
+    if h <= x
+    then detect (x :: t)
+    else false
+
+let halve x = x / 2
+
+let is_even n = 
+  (n mod 2) = 0
+
+let evens l = List.map is_even l
+
+let range n = (* ->  [0; 1; ... ] *)
+  let rec range_aux m l = 
+    if m = 0 
+    then 0 :: l
+    else range_aux (m - 1) (m :: l) 
+  in
+  if n <= 0
+  then []
+  else range_aux (n - 1) []
 
